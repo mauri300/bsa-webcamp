c969f42f6dcce3d4e347f38c62e5291a1cbcf720
mauri300 <mauriciojose_1999@hotmail.com>
2022-01-11 18:22:40 -0300

    plugins

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function(){
-    "use strict"
-
+  
+    
     var regalo = document.getElementById('regalo');
     document.addEventListener('DOMContentLoaded', function(){
         // console.log("listo");
@@ -129,3 +129,55 @@
         }
     });
 })();
+
+$(function(){
+
+    // Lettering
+    $('.nombre-sitio').lettering();
+
+    // Menu fijado
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > windowHeight){
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top': barraAltura+'px'});
+        } else {
+            $('.barra').removeClass('fixed');
+            $('body').css({'margin-top': '0px'});
+        }
+    });
+
+    // Menu Responsive
+    $('.menu-movil').on('click', function(){
+        $('.navegacion-principal').slideToggle();
+    });
+
+    // Programa de Conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function(){
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    });
+
+    // Animaciones para los números
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1500);
+
+    // Cuenta regresiva
+    $('.cuenta-regresiva').countdown('2023/1/10 09:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+});
